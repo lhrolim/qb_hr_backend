6dcd1db9789f2b90e3b57f5c27894c32d229a9c8
luiz.rolim <luizhenrique.rolim@gmail.com>
2018-05-18 13:54:32 -0300

    refactoring

---
+++ back/db/schema.rb
@@ -0,0 +1,55 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20180517172120) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.string "kind"
+    t.string "shift"
+    t.string "level"
+    t.string "description"
+    t.integer "max_periods"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "offers", force: :cascade do |t|
+    t.string "uuid"
+    t.string "description"
+    t.decimal "discount_percentage"
+    t.decimal "offered_price"
+    t.decimal "full_price"
+    t.integer "university_id"
+    t.integer "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_offers_on_course_id"
+    t.index ["university_id"], name: "index_offers_on_university_id"
+  end
+
+  create_table "universities", force: :cascade do |t|
+    t.string "name"
+    t.string "logo"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "universities_courses", id: false, force: :cascade do |t|
+    t.integer "university_id"
+    t.integer "course_id"
+    t.index ["course_id"], name: "index_universities_courses_on_course_id"
+    t.index ["university_id"], name: "index_universities_courses_on_university_id"
+  end
+
+end
---
+++ back/db/seeds.rb
@@ -0,0 +1,174 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require "securerandom"
+
+discount_arr = (10..70).step(5)
+fullprice_arr = (400..1000).step(50)
+
+un_arr = [
+  {
+    id: 1,
+    name: "UNIP",
+    logo: "https://s3.amazonaws.com/querobolsatemp-dev/logos/colorido/medium/25/logo_1489432787.png",
+  },
+  {
+    id: 2,
+    name: "ESTACIO",
+    logo: "https://s3.amazonaws.com/querobolsatemp-dev/logos/colorido/medium/25/logo_1489432787.png",
+  },
+  {
+    id: 3,
+    name: "UNINOVE",
+    logo: "https://s3.amazonaws.com/querobolsatemp-dev/logos/colorido/medium/25/logo_1489432787.png",
+  },
+  {
+    id: 4,
+    name: "UNISANTANA",
+    logo: "https://s3.amazonaws.com/querobolsatemp-dev/logos/colorido/medium/25/logo_1489432787.png",
+  },
+]
+
+course_arr = [
+
+  {
+    id: 1,
+    name: "Agronomia",
+    kind: "Presencial",
+    shift: "Manhã",
+    level: "Graduação",
+    max_periods: "6",
+    description: "Nível: Graduação Tradicional – Bacharelado / Graduação Tecnológica
+    Duração: 5 anos
+    Área: Ciências Agrárias
+    O profissional de Agronomia visa aumentar a produtividade agropecuária, do início ao fim da cadeia produtiva, por meio da preparação do solo para plantio, do controle de pragas e do armazenamento e conservação da produção, até a sua distribuição. Pode focar em aspectos econômicos, especializando-se no Agronegócio. Em um país que tem a agricultura como um dos pilares de sua economia, como é o Brasil, os agrônomos têm muito espaço para crescer, inclusive em órgãos públicos, na área de comércio exterior, indústrias de sementes, alimentos e adubos, além da área rural.",
+  },
+
+  {
+    id: 2,
+    name: "Agronomia",
+    kind: "Presencial",
+    shift: "Noite",
+    level: "Graduação",
+    max_periods: "6",
+    description: "Nível: Graduação Tradicional – Bacharelado / Graduação Tecnológica
+    Duração: 5 anos
+    Área: Ciências Agrárias
+    O profissional de Agronomia visa aumentar a produtividade agropecuária, do início ao fim da cadeia produtiva, por meio da preparação do solo para plantio, do controle de pragas e do armazenamento e conservação da produção, até a sua distribuição. Pode focar em aspectos econômicos, especializando-se no Agronegócio. Em um país que tem a agricultura como um dos pilares de sua economia, como é o Brasil, os agrônomos têm muito espaço para crescer, inclusive em órgãos públicos, na área de comércio exterior, indústrias de sementes, alimentos e adubos, além da área rural.",
+  },
+
+  {
+    id: 3,
+    name: "Engenharia de Telecomunicações",
+    kind: "Presencial",
+    shift: "Noite",
+    level: "Graduação",
+    max_periods: "10",
+    description: "O curso de Engenharia de Telecomunicações forma profissionais capacitados para o trabalho com sistemas de telecomunicações, planejando, projetando, operando e oferecendo manutenção.
+    Após a conclusão do curso, o profissional poderá trabalhar com recepção e transmissão de sinais, cuidar do sistema de cabeamento de dados e desenvolver novas tecnologias para o compartilhamento de informações.
+    No mercado de trabalho, o profissional de Engenharia de Telecomunicações pode atuar em centros de pesquisa de novas tecnologias, agências de fiscalização de serviços, fornecedoras de serviços de infraestrutura para telecomunicações, distribuidoras de tv a cabo, internet e telefonia.",
+  },
+
+  {
+    id: 4,
+    name: "Engenharia de Telecomunicações",
+    kind: "Presencial",
+    shift: "Tarde",
+    level: "Graduação",
+    max_periods: "10",
+    description: "O curso de Engenharia de Telecomunicações forma profissionais capacitados para o trabalho com sistemas de telecomunicações, planejando, projetando, operando e oferecendo manutenção.
+    Após a conclusão do curso, o profissional poderá trabalhar com recepção e transmissão de sinais, cuidar do sistema de cabeamento de dados e desenvolver novas tecnologias para o compartilhamento de informações.
+    No mercado de trabalho, o profissional de Engenharia de Telecomunicações pode atuar em centros de pesquisa de novas tecnologias, agências de fiscalização de serviços, fornecedoras de serviços de infraestrutura para telecomunicações, distribuidoras de tv a cabo, internet e telefonia.",
+  },
+
+  {
+    id: 5,
+    name: "Engenharia de Telecomunicações",
+    kind: "EAD",
+    shift: "Virtual",
+    level: "Graduação",
+    max_periods: "10",
+    description: "O curso de Engenharia de Telecomunicações forma profissionais capacitados para o trabalho com sistemas de telecomunicações, planejando, projetando, operando e oferecendo manutenção.
+    Após a conclusão do curso, o profissional poderá trabalhar com recepção e transmissão de sinais, cuidar do sistema de cabeamento de dados e desenvolver novas tecnologias para o compartilhamento de informações.
+    No mercado de trabalho, o profissional de Engenharia de Telecomunicações pode atuar em centros de pesquisa de novas tecnologias, agências de fiscalização de serviços, fornecedoras de serviços de infraestrutura para telecomunicações, distribuidoras de tv a cabo, internet e telefonia.",
+  },
+
+  {
+    id: 6,
+    name: "Engenharia de Telecomunicações",
+    kind: "EAD",
+    shift: "Virtual",
+    level: "Graduação",
+    max_periods: "10",
+    description: "O curso de Engenharia de Telecomunicações forma profissionais capacitados para o trabalho com sistemas de telecomunicações, planejando, projetando, operando e oferecendo manutenção.
+    Após a conclusão do curso, o profissional poderá trabalhar com recepção e transmissão de sinais, cuidar do sistema de cabeamento de dados e desenvolver novas tecnologias para o compartilhamento de informações.
+    No mercado de trabalho, o profissional de Engenharia de Telecomunicações pode atuar em centros de pesquisa de novas tecnologias, agências de fiscalização de serviços, fornecedoras de serviços de infraestrutura para telecomunicações, distribuidoras de tv a cabo, internet e telefonia.",
+  },
+
+  {
+    id: 7,
+    name: "Engenharia de Computação",
+    kind: "Graduação",
+    shift: "Manhã",
+    level: "Graduação",
+    max_periods: "10",
+    description: "O curso de Engenharia de Computação forma profissionais habilitados para o desenvolvimento e planejamento de softwares e hardwares. Ao final do curso, o profissional poderá projetar, programar e gerenciar sistemas de softwares, além de criar e projetar hardwares.
+    No mercado de trabalho, há espaço para o profissional de Engenharia de Computação em grandes empresas, criando e gerenciando sistemas, ou desenvolvendo novas máquinas industriais e novas tecnologias. Há ainda a opção de se manter no meio científico, dando aulas em universidades e desenvolvendo pesquisas.",
+  },
+
+  {
+    id: 8,
+    name: "Engenharia de Computação",
+    kind: "Graduação",
+    shift: "Noite",
+    level: "Graduação",
+    max_periods: "10",
+    description: "O curso de Engenharia de Computação forma profissionais habilitados para o desenvolvimento e planejamento de softwares e hardwares. Ao final do curso, o profissional poderá projetar, programar e gerenciar sistemas de softwares, além de criar e projetar hardwares.
+      No mercado de trabalho, há espaço para o profissional de Engenharia de Computação em grandes empresas, criando e gerenciando sistemas, ou desenvolvendo novas máquinas industriais e novas tecnologias. Há ainda a opção de se manter no meio científico, dando aulas em universidades e desenvolvendo pesquisas.",
+  },
+
+  {
+    id: 9,
+    name: "Engenharia de Computação",
+    kind: "Graduação",
+    shift: "Noite",
+    level: "Graduação",
+    max_periods: "10",
+    description: "O curso de Engenharia Florestal forma profissionais com consciência ambiental para promover o manejo sustentável de florestas, tendo em mente a preservação e a exploração consciente. Após a conclusão do curso, o engenheiro pode desenvolver projetos de recuperação de áreas degradadas, planos de exploração de florestas e conservação de parques e reservas naturais.
+    No mercado de trabalho, o profissional de Engenharia Florestal pode ser encontrado trabalhando em órgãos ambientais e em empresas privadas e públicas, realizando vistorias, encarregando-se de laudos técnicos, estudos e relatórios de impacto ambiental.",
+  },
+
+]
+
+ActiveRecord::Base.transaction do
+  un_arr = University.create(un_arr)
+  course_arr = Course.create(course_arr)
+
+  un_arr.each do |un|
+    (1..3).each do |i|
+      un.course << course_arr[(i * un.id) % 9]
+    end
+  end
+
+  #creating 300 offers
+  (1..300).each do |i|
+    full_price = fullprice_arr.to_a.sample
+    discount = discount_arr.to_a.sample
+    offered_price = full_price - (full_price * discount / 100)
+    Offer.create(
+      {
+        uuid: SecureRandom.uuid,
+        description: "this is a very large description for the offer, please be aware that this is not used at the listing",
+        discount_percentage: discount,
+        full_price: full_price,
+        offered_price: offered_price,
+        university: un_arr.sample,
+        course: course_arr.sample,
+      }
+    )
+  end
+end
