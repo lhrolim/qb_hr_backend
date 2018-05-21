56d247fb91f7250747f17b225b96b7b7bed27695
luiz.rolim <luizhenrique.rolim@gmail.com>
2018-05-21 10:35:38 -0300

    fixing .gitignore and updating store

---
+++ front/infra/core/store.js
@@ -0,0 +1,33 @@
+import { createStore,combineReducers,applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+
+/** Authentication module */
+import offerReducer from '../../app/usecases/offer/offerreducer'
+import navigationReducer from '../navigation/navigationReducer'
+
+import {createLogger} from 'redux-logger';
+
+import {
+    createReactNavigationReduxMiddleware,
+  } from 'react-navigation-redux-helpers';
+
+const reactNavigationMiddleware = createReactNavigationReduxMiddleware(
+    "root",
+    state => state.navigationReducer,
+  );
+
+const logger = createLogger();
+
+const combinedReducers = combineReducers({
+    offerReducer,
+    navigationReducer
+})
+
+const middlewares = [thunkMiddleware,logger,reactNavigationMiddleware ]
+
+
+// Centralized application state
+// For more information visit http://redux.js.org/
+const store = createStore(combinedReducers, applyMiddleware(...middlewares));
+
+export default store;
